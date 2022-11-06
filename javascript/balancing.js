536b0428ad648957856276d8325312364a8aced3
Jeff Marks <jeff.t.marks@gmail.com>
2022-11-06 08:59:39 -0500

    done with assignment

--- javascript/balancing.js
+++ javascript/balancing.js
@@ -8,12 +8,25 @@ class Node {
 
 // list = [3, 5, 6, 9, 10, 20]
 function threeToTwenty() {
+	const left_l2 = new Node(3);
+	const left_r2 = new Node(6);
+	const left = new Node(5, left_l2, left_r2);
 
+	const right_l2 = new Node(10);
+	const right = new Node(20, right_l2);
+	
+	return new Node(9, left, right);
 }
 
 // list = [10, 11, 30, 100, 200]
 function tenToTwoHundred() {
+	const left_l2 = new Node(10);
+	const left = new Node(11, left_l2);
 
+	const right_r2 = new Node(200);
+	const right = new Node(100, null, right_r2);
+
+	return new Node(30, left, right);
 }
 
 if (require.main === module) {
